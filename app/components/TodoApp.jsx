f6115e4e855b5ec91eb1fccb5f76d79ef38c6ec0
mayankamencherla <mayank.amencherla@razorpay.com>
2017-04-16 16:06:26 +0530

    Adding new todo to state and added test case for the same

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var uuid = require('node-uuid');
 
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
@@ -11,26 +12,34 @@ var TodoApp = React.createClass({
             showCompleted: false,
             todos: [
                 {
-                    id: 1,
+                    id: uuid(),
                     text: "Walk the dog"
                 },
                 {
-                    id: 2,
+                    id: uuid(),
                     text: "Clean the yard"
                 },
                 {
-                    id: 3,
+                    id: uuid(),
                     text: "Make a protein shake"
                 },
                 {
-                    id: 4,
+                    id: uuid(),
                     text: "Watch Andrew's lectures"
                 },
             ],
         };
     },
     handleAddTodo: function(text) {
-        alert('new todo : ' + text);
+        this.setState({
+            todos: [
+                ...this.state.todos,
+                {
+                    id: uuid(),
+                    text: text
+                }
+            ]
+        });
     },
     handleSearch: function(searchText, showCompleted) {
         this.setState({
