d8d9ac73d0d80819b921051cf744e537c705b332
mayankamencherla <mayank.amencherla@razorpay.com>
2017-04-16 15:28:45 +0530

    Added testing for the filter component

---
+++ tests/components/TodoSearch.test.jsx
@@ -0,0 +1,29 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
+
+var TodoSearch = require('TodoSearch');
+
+describe('TodoSearch', () => {
+    it('should exist', () => {
+        expect(TodoSearch).toExist();
+    });
+
+    it('should call onSearch with correct arguments', () => {
+        var searchText = 'Walk the dog';
+        var showCompleted = true;
+
+        var spy = expect.createSpy();
+        var todoSearch = TestUtils.renderIntoDocument(<TodoSearch onSearch={spy} />);
+
+        todoSearch.refs.searchText.value = searchText;
+        todoSearch.refs.showCompleted.checked = showCompleted;
+
+        TestUtils.Simulate.change(todoSearch.refs.searchText);
+        TestUtils.Simulate.change(todoSearch.refs.showCompleted);
+        
+        expect(spy).toHaveBeenCalledWith(searchText, showCompleted);
+    });
+});
