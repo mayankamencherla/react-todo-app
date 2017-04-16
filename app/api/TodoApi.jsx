2f3b274b947743fa67d5510f500cafd7e25d5dae
mayankamencherla <mayank.amencherla@razorpay.com>
2017-04-16 20:00:21 +0530

    Added filters for to search todo by string match or completed status

--- app/api/TodoApi.jsx
+++ app/api/TodoApi.jsx
@@ -18,5 +18,23 @@ module.exports = {
         }
 
         return ($.isArray(todos) === true ? todos : []);
+    },
+    filterTodos: function(todos, showCompleted, searchText) {
+        var filteredTodos = todos;
+
+        // If the todo is incomplete or if the showCompleted 
+        // button is clicked, then show the todo
+        filteredTodos = filteredTodos.filter((todo) => {
+            return !todo.completed || showCompleted;
+        });
+
+        // filter by search text
+        filteredTodos = filteredTodos.filter((todo) => {
+            return todo.text.toLowerCase().includes(searchText.toLowerCase());
+        });
+
+        // sort and move incomplete todos to the top
+
+        return filteredTodos;
     }
 };
