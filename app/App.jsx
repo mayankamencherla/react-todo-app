69e7d15c242a438a7a04ce83354a6e203cbf96ef
mayankamencherla <mayank.amencherla@razorpay.com>
2017-04-23 15:42:53 +0530

    Added redux to the todos app

--- app/App.jsx
+++ app/App.jsx
@@ -4,12 +4,23 @@ var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
 var TodoApp = require('TodoApp');
 
+var actions = require('actions');
+var store = require('configureStore').configure();
+
 // Load foundation
 require('style!css!foundation-sites/dist/foundation.min.css');
 $(document).foundation();
 
 // Load custom css
 require('!style!css!sass!applicationStyles');
+
+store.subscribe(() => {
+    console.log('newState', store.getState());
+});
+
+store.dispatch(actions.addTodo('Clean the yard'));
+store.dispatch(actions.setSearchText('yard'));
+store.dispatch(actions.toggleShowCompleted());
 
 ReactDOM.render(
     <Router history={hashHistory}>
