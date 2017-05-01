17e3a9f960cd106d4dc222c8c554bf1354bd2c62
mayankamencherla <mayank.amencherla@razorpay.com>
2017-05-02 00:23:57 +0530

    Move firebase credentials to environment variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,12 +2,12 @@ import firebase from 'firebase';
 
 try {
     var config = {
-        apiKey: "AIzaSyDm0KX7sLs-GOQqFS0sqqIu2U0pj_LkoUY",
-        authDomain: "mayank-todoapp.firebaseapp.com",
-        databaseURL: "https://mayank-todoapp.firebaseio.com",
-        projectId: "mayank-todoapp",
-        storageBucket: "mayank-todoapp.appspot.com",
-        messagingSenderId: "481169471862"
+        apiKey: process.env.API_KEY,
+        authDomain: process.env.AUTH_DOMAIN,
+        databaseURL: process.env.DATABASE_URL,
+        projectId: process.env.PROJECT_ID,
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGING_SENDER_ID
     };
 
     firebase.initializeApp(config);
