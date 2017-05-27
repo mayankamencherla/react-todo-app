7688475753cc2796cb89f519180f30552c18ae5a
mayankamencherla <mayank.amencherla@razorpay.com>
2017-05-27 20:11:35 +0530

    Switching to /todos directly if already logged in

--- app/App.jsx
+++ app/App.jsx
@@ -30,12 +30,28 @@ require('!style!css!sass!applicationStyles');
 
 store.dispatch(actions.startAddTodos());
 
+// Sending user back to login page if user not logged in
+var requireLogin = (nextState, replace, next) => {
+    if (!firebase.auth().currentUser) {
+        replace('/');
+    }
+    next();
+};
+
+// Redirect to todos page if user already logged in 
+var redirectIfLoggedIn = (nextState, replace, next) => {
+    if (firebase.auth().currentUser) {
+        replace('/todos');
+    }
+    next();
+};
+
 ReactDOM.render(
     <Provider store={store}>
         <Router hitory={hashHistory}>
             <Route path="/">
-                <Route path="todos" component={TodoApp} />
-                <IndexRoute component={Login} />
+                <Route path="todos" component={TodoApp} onEnter={requireLogin} />
+                <IndexRoute component={Login} onEnter={redirectIfLoggedIn} />
             </Route>
         </Router>
     </Provider>,
